975d49deea41cdf84a30a598065ddf916dc0f634
kjfsoul <kjfsoul@gmail.com>
2025-05-09 07:16:39 -0400

    client/src/main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import "./styles/cosmic-background.css";
+import "./styles/card-flip.css";
 import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
